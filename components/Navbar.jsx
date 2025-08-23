70e70dfbd906682ffdc4337c6df801fef44fe402
Shrey-Patel-05 <shrey.anup.patel@gmail.com>
2025-08-23 15:31:17 +0100

    initial commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className="fixed top-0 left-0 right-0 mt-8 z-30 hidden md:flex justify-center text-sm">
+                <div className="bg-transparent backdrop-blur-3xl  border py-4 px-3 rounded-md">
+                    <ul className="flex space-x-2 font-title font-bold shadow-2xl">
+                        <li><a href="/" className="text-primary hover:text-primary/70 p-2">Landing</a></li>
+                        <li><a href="Home" className="mr-3 text-primary hover:text-primary/70">Home</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
