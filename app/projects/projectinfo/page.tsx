e3c89d6f88fcbf9382f410950b4a342f9f4c758c
Shrey-Patel-05 <shrey.anup.patel@gmail.com>
2025-09-01 02:28:15 +0100

    made searchParams potentially a promise

--- app/projects/projectinfo/page.tsx
+++ app/projects/projectinfo/page.tsx
@@ -2,10 +2,16 @@ import { fetchProjectByID } from "@/lib/data";
 import PageContents from "@/app/projects/projectinfo/PageContents";
 import type { project } from "@/lib/definitions";
 
-type Props = { searchParams: { id: string } };
+type Props = {
+  searchParams: { id: string | undefined } | Promise<{ id: string | undefined }>;
+};
 
 export default async function Page({ searchParams }: Props) {
-  const project: project | null = await fetchProjectByID(searchParams.id);
+  const { id } = await searchParams; // <-- await it here
+
+  if (!id) return <div>Project ID missing</div>;
+
+  const project: project | null = await fetchProjectByID(id);
 
   if (!project) return <div>Project not found</div>;
 
